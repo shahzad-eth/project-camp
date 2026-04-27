401cf2cc5e20a4a8e38bd7f163301334108aa633
Shahzad Rizvi <shahzad@imantix.ai>
2026-04-27 12:38:27 +0530

    feat: add and initialize express in the code base.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv"
-
+import express from "express"
 dotenv.config({
     path: "./.env"
 })
 
-console.log("Faaaaaaaaaaaaaaaaa")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("Surprize ^_^")
+})
+app.listen(port, () => {
+    console.log(`Faaaaaaaaaaaaaaaaa, http://localhost:${port}`)
+})
