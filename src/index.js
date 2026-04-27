b69bcb6432f012d6a67c78b3c5112929f9d1b76d
Shahzad Rizvi <shahzad@imantix.ai>
2026-04-27 13:00:31 +0530

    feat: (Refractor) Add `app.js` and seperate express initialization from index.`js`.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import dotenv from "dotenv"
-import express from "express"
+import app from "./app.js"
 dotenv.config({
     path: "./.env"
 })
 
-const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-    res.send("Surprize ^_^")
-})
 app.listen(port, () => {
     console.log(`Faaaaaaaaaaaaaaaaa, http://localhost:${port}`)
 })
