0607982f86e918cafcd439c174f483662911f979
Shahzad Rizvi <shahzad@imantix.ai>
2026-05-26 12:55:57 +0530

    feat: Define `verifyJWT` function in `auth.middleware.js`.

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { User } from "../models/User.model.js"
+import { asyncHandler } from "../utils/async-handler.js"
+import { ApiError } from "../utils/api-error.js"
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    //Get JWT from cookie/header
+    const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized Request")
+    }
+
+    //decode the token
+    try {
+        const decodedToken = jwt.sign(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken -accessToken -emailVerificationToken -emailVerificationExpiry"
+        )
+
+        if (!user) {
+            throw new ApiError(401, "Invalid access token")
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, "Invalid access token")
+    }
+})
