44920b38ffd358ff0201ba50991ae8e03a6f1aad
shahzad-eth <rizvishahzad44@gmail.com>
2026-07-23 01:43:04 +0530

    feat: Define boiler plate code for tasks controller in `/src/controller/task.controller.js`.

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,45 @@
+import { User } from "../models/User.model.js";
+import { Project } from "../models/project.model.js";
+import { Task } from "../models/task.model.js";
+import { SubTask } from "../models/subtask.model.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import mongoose from "mongoose";
+import { AvailableUser, UserRolesEnum } from "../utils/constants.js";
+
+const getTasks = asyncHandler(async (req, res) => {
+  //
+});
+const createTask = asyncHandler(async (req, res) => {
+  //
+});
+const getTaskById = asyncHandler(async (req, res) => {
+  //
+});
+const updateTask = asyncHandler(async (req, res) => {
+  //
+});
+const deleteTask = asyncHandler(async (req, res) => {
+  //
+});
+const createSubTask = asyncHandler(async (req, res) => {
+  //
+});
+const updateSubTask = asyncHandler(async (req, res) => {
+  //
+});
+const deleteSubTask = asyncHandler(async (req, res) => {
+  //
+});
+
+export {
+  createTask,
+  getTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  createSubTask,
+  updateSubTask,
+  deleteSubTask,
+};
