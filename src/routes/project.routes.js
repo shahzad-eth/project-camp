6509bfa0b3217a170ba9974e74a442250407591f
shahzad-eth <rizvishahzad44@gmail.com>
2026-07-23 01:42:04 +0530

    fix: prettier formattings in all files.

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,44 +1,58 @@
 import { Router } from "express";
 import {
-    getProjects,
-    getProjectById,
-    createProject,
-    updateProject,
-    deleteProject,
-    addMembersToProject,
-    getProjectMembers,
-    updateMemberRole,
-    deleteMember,
+  getProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+  addMembersToProject,
+  getProjectMembers,
+  updateMemberRole,
+  deleteMember,
 } from "../controllers/project.controller.js";
 import {
-    createProjectValidator,
-    addMemberToProjectValidator,
+  createProjectValidator,
+  addMemberToProjectValidator,
 } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { verifyJWT, validateProjectPermission } from "../middlewares/auth.middleware.js";
+import {
+  verifyJWT,
+  validateProjectPermission,
+} from "../middlewares/auth.middleware.js";
 import { AvailableUser, UserRolesEnum } from "../utils/constants.js";
 
 const router = Router();
-router.use(verifyJWT) // all following router will use this validator as middleware before running any method
+router.use(verifyJWT); // all following router will use this validator as middleware before running any method
 
 router
-    .route("/")
-    .get(getProjects)
-    .post(createProjectValidator(), validate, createProject)
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject);
 
 router
-    .route("/:projectId")
-    .get(validateProjectPermission(AvailableUser), getProjectById)
-    .put(validateProjectPermission([UserRolesEnum.ADMIN]), createProjectValidator(), validate, updateProject)
-    .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject)
+  .route("/:projectId")
+  .get(validateProjectPermission(AvailableUser), getProjectById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
 
 router
-    .route("/:projectId/members")
-    .get(getProjectMembers)
-    .post(validateProjectPermission([UserRolesEnum.ADMIN]), addMemberToProjectValidator(), validate, addMembersToProject)
+  .route("/:projectId/members")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProjectValidator(),
+    validate,
+    addMembersToProject,
+  );
 
 router
-    .route("/:projectId/members/:userId")
-    .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
-    .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember)
-export default router
+  .route("/:projectId/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
+
+export default router;
